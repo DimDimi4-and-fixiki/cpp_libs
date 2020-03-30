0740f2e56603304e6d1244a9090517beff04d771
DimDimi4-and-fixiki <40062148+DimDimi4-and-fixiki@users.noreply.github.com>
2020-03-30 18:12:12 +0300

    Бинпоиск

---
+++ binsearch/binsearch.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+inline int binsearch (int* array, int left, int right, int value) 
+{   
+    while (right - left > 3) // уменьшаем границы поиска до длины 3
+    {
+        int mid = (left + right) / 2;
+        if (array[mid] == value) { // нашли нужный индекс
+            return mid;
+        }
+        if (array[mid] < value) { // нужное значение правее
+            left = mid + 1;
+        }
+        if  (array[mid] > value) { // нужное значение левее
+            right = mid - 1;
+        }
+    }
+}
+
