269519ca1522e764d3412ad9ae6feb448e5638aa
DimDimi4-and-fixiki <40062148+DimDimi4-and-fixiki@users.noreply.github.com>
2020-03-30 18:31:56 +0300

    Create segment_tree.cpp

---
+++ segment tree/segment_tree.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class SegmentTree  // класс дерева отрезков
+{
+    public:
+    static const int N = 1e5; // число элементов в массиве данных
+    int max_tree[4 * N], min_tree[4 * N], sum_tree[4 * N]; // массивы для дерева
+
+    inline void build(int v, int left, int right, int* a) // построение дерева
+    {
+        if (left == right) {  // случай, когда спустились в самый низ
+            max_tree[v] = a[v];
+            min_tree[v] = a[v];
+            sum_tree[v] = a[v];
+        }
+        else {  // берем середину и рекурсивно строим на двух половинах
+            int mid = (left + right) / 2;
+            build(v * 2, left, mid, a);
+            build(v * 2 + 1, mid + 1, right, a);
+            max_tree[v] = max(max_tree[v * 2], max_tree[v * 2 + 1]);  // обновляем максимум
+            min_tree[v] = min(min_tree[v * 2], min_tree[v * 2 + 1]);  // обновляем минимум
+            sum_tree[v] = sum_tree[v * 2] + sum_tree[v * 2 + 1];  // обновляем сумму
+        }
+    }
+
+    inline void update(int v, int left, int right, int idx, int value) // обновление в вершине
+    {
+        if (left == right) {  //  обновляем в листе
+            max_tree[v] = value;
+            min_tree[v] = value;
+            sum_tree[v] = value;
+            return;
+        }
+        int mid = (left + right) / 2;
+        if (idx <= mid) {  // спускаемся по дереву влево
+            update(v * 2, left, mid, idx, value);
+        }
+        else {  // спускаемся по дереву вправо
+            update(v * 2 + 1, mid + 1, right, idx, value);
+        }
+        min_tree[v] = min(min_tree[v * 2], min_tree[v * 2 + 1]);  // считаем минимум в вершине
+        max_tree[v] = max(max_tree[v * 2], max_tree[v * 2 + 1]);  // считаем максимум в вершине
+        max_tree[v] = sum_tree[v * 2] + sum_tree[v * 2 + 1];  // считаем сумму в вершине
+    }
+
+    inline int get_max(int v, int left, int right, int l, int r) // максимум на отрезке
+    {
+        if (l > right || r < left || l > r){  // вышли за отрезок
+            return -1e9;
+        }
+        if (l == left && r == right) {  // попали в нужную вершину
+            return max_tree[v];
+        }
+        else {  // разбиваем на два отрезка и рекурентно считаем для каждого
+            int mid = (left + right) / 2;
+            return max(get_max(2 * v, left, mid, l, min(r, mid)), 
+            get_max(2 * v + 1, mid + 1, right, max(l, mid + 1), r));
+        }
+    }
+
+    inline int get_min(int v, int left, int right, int l, int r) // минимум на отрезке
+    {
+        if (l > right || r < left || l > r){  // вышли за отрезок
+            return 1e9;
+        }
+        if (l == left && r == right) {  // попали в нужную вершину
+            return min_tree[v];
+        }
+        else {  // разбиваем на два отрезка и рекурентно считаем для каждого
+            int mid = (left + right) / 2;
+            return min(get_min(2 * v, left, mid, l, min(r, mid)), 
+            get_min(2 * v + 1, mid + 1, right, max(l, mid + 1), r));
+        }
+    }
+
+    inline int get_sum(int v, int left, int right, int l, int r) // сумма на отрезке
+    {
+        if (l > right || r < left || l > r){  // вышли за отрезок
+            return 0;
+        }
+        if (l == left && r == right) {  // попали в нужную вершину
+            return sum_tree[v];
+        }
+        else {  // разбиваем на два отрезка и рекурентно считаем для каждого
+            int mid = (left + right) / 2;
+            return get_sum(2 * v, left, mid, l, min(r, mid)) + 
+            get_sum(2 * v + 1, mid + 1, right, max(l, mid + 1), r);
+        }
+    }
+}
